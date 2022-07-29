b70ba5e86402f9fe9fb821543d6a30c4f606b2ea
Junha Park <vjunhav@gmail.com>
2022-07-30 03:27:51 +0900

    New : ERC20Trackable Wrapper contract / TelescopeToken contract created.
    Hotfix : TelescopeToken token name fixed.

---
+++ contracts/ERC20Trackable.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.8.0;
+
+import "./cores/ERC20.sol";
+import "./cores/draft-ERC20Permit.sol";
+import "./ERC20Votes.sol";
+
+
+abstract contract ERC20Trackable is ERC20, ERC20Permit, ERC20Votes {
+    // constructor() ERC20("JWDToken", "JWD") ERC20Permit("JWDToken") {}
+
+    // The functions below are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
+}
---
+++ contracts/TelescopeToken.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.0;
+
+import "./ERC20Trackable.sol";
+import "./cores/ERC20.sol";
+
+// minting 예제
+contract TelescopeToken is ERC20Trackable {
+  uint256 INITIAL_SUPPLY = 100000;  // 100000 TEL를 발행
+
+constructor() ERC20("TelescopeToken", "TEL") ERC20Permit("TEL") {
+    _mint(msg.sender, INITIAL_SUPPLY * 10**uint(decimals()));
+
+}
+}
