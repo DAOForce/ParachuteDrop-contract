2a8d82f2560e39487c27c61d70eac986311457eb
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-30 20:22:30 +0900

    refactor: init relevant information on contract

--- contracts/TelescopeToken.sol
+++ contracts/TelescopeToken.sol
@@ -7,7 +7,12 @@ import "./cores/ERC20.sol";
 contract TelescopeToken is ERC20Trackable {
     uint256 INITIAL_SUPPLY = 100000;  // 100000 TEL를 발행
 
-    constructor() ERC20("TelescopeToken", "TEL") ERC20Trackable ("TEL") {
+    constructor(
+        string memory _name, string memory _ticker,
+        string memory _DAOName, string memory _intro,
+        string memory _image, string memory _link
+    ) ERC20(_name, _ticker, _DAOName, _intro, _image, _link) ERC20Trackable (_ticker)
+    {
         _mint(msg.sender, INITIAL_SUPPLY * 10 ** uint(decimals()));
     }
 }
