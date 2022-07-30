a29cffae21069b5c3da5eb66fc0910a9b0796525
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-07-30 19:27:22 +0900

    refactor: move constructor logic to ERC20permit

--- contracts/ERC20Trackable.sol
+++ contracts/ERC20Trackable.sol
@@ -4,27 +4,19 @@ import "./cores/ERC20.sol";
 import "./cores/draft-ERC20Permit.sol";
 import "./ERC20VotesComp.sol";
 
-
 abstract contract ERC20Trackable is ERC20, ERC20Permit, ERC20VotesComp {
 
-    function _afterTokenTransfer(address from, address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
+    constructor(string memory name) ERC20Permit(name, "1") {}
+
+    function _afterTokenTransfer(address from, address to, uint256 amount) internal override (ERC20, ERC20Votes) {
         super._afterTokenTransfer(from, to, amount);
     }
 
-    function _mint(address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
+    function _mint(address to, uint256 amount) internal override(ERC20, ERC20Votes) {
         super._mint(to, amount);
     }
 
-    function _burn(address account, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
+    function _burn(address account, uint256 amount) internal override(ERC20, ERC20Votes) {
         super._burn(account, amount);
     }
 }
--- contracts/TelescopeToken.sol
+++ contracts/TelescopeToken.sol
@@ -5,10 +5,9 @@ import "./cores/ERC20.sol";
 
 // minting 예제
 contract TelescopeToken is ERC20Trackable {
-  uint256 INITIAL_SUPPLY = 100000;  // 100000 TEL를 발행
+    uint256 INITIAL_SUPPLY = 100000;  // 100000 TEL를 발행
 
-constructor() ERC20("TelescopeToken", "TEL") ERC20Permit("TEL") {
-    _mint(msg.sender, INITIAL_SUPPLY * 10**uint(decimals()));
-
-}
+    constructor() ERC20("TelescopeToken", "TEL") ERC20Trackable ("TEL") {
+        _mint(msg.sender, INITIAL_SUPPLY * 10**uint(decimals()));
+    }
 }
